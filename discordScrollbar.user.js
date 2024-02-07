417fc489269ea2bf17678182dc8b6bb0caa7870d
techflashes <70425017+techflashes@users.noreply.github.com>
2024-02-07 10:42:34 -0500

    Fixed discordScrollbar.user.js

--- discordScrollbar.user.js
+++ discordScrollbar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discord Scrollbar
 // @namespace    https://techflashes.pythonanywhere.com/
-// @version      0.3
+// @version      1.0
 // @description  Creates a scrollbar for the server list
 // @author       TechFlashes
 // @match        *://discord.com/channels/*
@@ -11,38 +11,29 @@
 // ==/UserScript==
 
 (function() {
-const cssTemplateString = `
-.fixedScrollbar::-webkit-scrollbar {
-    width: 8px !important;
-}
-.fixedScrollbar::-webkit-scrollbar-corner {
-    background-color: transparent;
-}
-.fixedScrollbar::-webkit-scrollbar-thumb {
-    background-clip: padding-box;
-    border: 2px solid transparent;
-    border-radius: 4px;
-    background-color: var(--background-primary);
-    min-height: 80px;
-}
-.fixedScrollbar::-webkit-scrollbar-track {
-    border-color: transparent;
-    background-color: transparent;
-    border: 2px solid transparent;
-}
-.fixedScrollbar {
-    scrollbar-width: thin;
-}
-`;
-const styleTag = document.createElement("style");
-styleTag.innerHTML = cssTemplateString;
-document.head.insertAdjacentElement('beforeend', styleTag);
-
-let recheckElement = setInterval(() => {
-    let serverList = document.querySelector("[aria-label='Servers']");
-    if (serverList && serverList.parentElement && !serverList.parentElement.classList.contains("fixedScrollbar")) {
-        serverList.parentElement.classList.add("fixedScrollbar");
-    }
-}, 300);
-
+	const cssTemplateString = `
+	div[class*='none_']{
+		scrollbar-width: thin !important;
+		scrollbar-color: var(--bg-overlay-3,var(--background-secondary))var(--scrollbar-thin-track);
+	}
+	div[class*='none_']::-webkit-scrollbar {
+		width: 8px !important;
+	}
+	div[class*='none_']::-webkit-scrollbar-corner {
+		background-color: transparent !important;
+	}
+	div[class*='none_']::-webkit-scrollbar-thumb {
+		background-clip: padding-box !important;
+		border: 2px solid transparent !important;
+		border-radius: 4px !important;
+		background-color: var(--scrollbar-thin-thumb) !important;
+		min-height: 40px !important;
+	}
+	div[class*='none_']::-webkit-scrollbar-track {
+		visibility: hidden !important;
+	}
+	`;
+	const styleTag = document.createElement("style");
+	styleTag.innerHTML = cssTemplateString;
+	document.head.insertAdjacentElement('beforeend', styleTag);
 })();
